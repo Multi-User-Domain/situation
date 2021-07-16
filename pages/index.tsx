6a863c03dc03f89e2d7d5840d21c4630f5cfc948
Calum Mackervoy <c.mackervoy@gmail.com>
2021-07-16 11:50:30 +0200

    initialized UI with login form

---
+++ pages/index.tsx
@@ -0,0 +1,52 @@
+import { useSession } from "@inrupt/solid-ui-react/dist";
+
+import {
+  Flex,
+  Box,
+  Button,
+  Container,
+  useDisclosure,
+  Heading
+} from "@chakra-ui/react";
+
+import {
+  LogoutButton,
+} from "@inrupt/solid-ui-react";
+
+import LoginForm from "../components/loginForm";
+
+export default function Home(): React.ReactElement {
+  const { session } = useSession();
+  const { webId } = session.info;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  if (!session.info.isLoggedIn) {
+    return <LoginForm />;
+  }
+
+  const header = (
+    <Container marginBottom={4} textAlign="left">
+      <Flex>
+        <Box alignSelf="flex-start">
+          <Heading>Situation</Heading>
+        </Box>
+
+        <Box w="100vw"></Box>
+
+        <Box alignSelf="flex-end">
+          <LogoutButton>
+            <Button variant="contained">
+                Log&nbsp;out
+            </Button>
+          </LogoutButton>
+        </Box>
+      </Flex>
+    </Container>
+  );
+
+  return (
+    <Container>
+        {header}
+    </Container>
+    );
+}
