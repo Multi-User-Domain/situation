d36d7407bc8d8089b7f73aad03917ff0fbc33258
Calum Mackervoy <c.mackervoy@gmail.com>
2021-08-07 23:24:58 +0200

    prototype ThingView component

---
+++ components/thingView/index.tsx
@@ -0,0 +1,23 @@
+import { Container, Center } from "@chakra-ui/react";
+import { Thing, getStringNoLocale } from "@inrupt/solid-client";
+import { RDF } from "@inrupt/lit-generated-vocab-common";
+import { ThingDepiction, getThingName } from "@multi-user-domain/mud-lib";
+
+/*
+*   A component for rendering a generic Thing
+*/
+
+interface IThingView {
+    thing: Thing;
+}
+
+export default function ThingView({thing} : IThingView) : React.ReactElement {
+
+    return (
+        <Container>
+            <Center><ThingDepiction thing={thing}></ThingDepiction></Center>
+            <Center><h1>{getThingName(thing)}</h1></Center>
+            <Center><h3>{getStringNoLocale(thing, RDF.type)}</h3></Center>
+        </Container>
+    );
+}
