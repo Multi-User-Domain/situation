f1f711fbf36ebb3281dcedb106a9b02c47a2b02f
Calum Mackervoy <c.mackervoy@gmail.com>
2023-01-25 18:59:39 +0100

    ChakraProvider in home page for styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,52 +5,76 @@ import {
   Box,
   Button,
   Container,
-  Heading
+  Heading,
+  ChakraProvider,
+  extendTheme
 } from "@chakra-ui/react";
 
 import {
   LogoutButton,
 } from "@inrupt/solid-ui-react";
 
-import SocialFeed from "../components/socialFeed";
 import { LoginForm } from "../components/loginForm";
 import { MudAccountProvider } from "../context/mudAccountContext";
+
+const theme = extendTheme({
+  components:  {
+    Container: {
+      baseStyle: {
+        padding: "0px"
+      },
+    },
+  },
+});
 
 export default function Home(): React.ReactElement {
   const { session } = useSession();
 
-  if (!session.info.isLoggedIn) {
-    return <LoginForm />;
-  }
-
-  const header = (
-    <Container marginBottom={4} textAlign="left">
-      <Flex>
-        <Box alignSelf="flex-start">
-          <Heading>Situation</Heading>
-        </Box>
-
-        <Box w="100vw"></Box>
-
-        <Box alignSelf="flex-end">
-          <LogoutButton>
-            <Button variant="contained">
-                Log&nbsp;out
-            </Button>
-          </LogoutButton>
-        </Box>
-      </Flex>
-    </Container>
+  let content = null;
+  if (!session.info.isLoggedIn) content = (
+    <LoginForm />
   );
 
-  return (
-    <MudAccountProvider>
+  else {
+    const header = (
+      <Container marginBottom={4} textAlign="left">
+        <Flex>
+          <Box alignSelf="flex-start">
+            <Heading>Situation</Heading>
+          </Box>
+  
+          <Box w="100vw"></Box>
+  
+          <Box alignSelf="flex-end">
+            <LogoutButton>
+              <Button variant="contained">
+                  Log&nbsp;out
+              </Button>
+            </LogoutButton>
+          </Box>
+        </Flex>
+      </Container>
+    );
+
+    content = (
+      <>
       <Container>
         {header}
       </Container>
       <Container>
         
       </Container>
-    </MudAccountProvider>
+      </>
+    );
+  }
+
+  
+
+  return (
+    <ChakraProvider theme={theme}>
+      <MudAccountProvider>
+        {content}
+      </MudAccountProvider>
+    </ChakraProvider>
     );
 }
